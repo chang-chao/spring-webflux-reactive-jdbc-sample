9f649a3582d7ad08c5d1332ac1e91fcae311888e
chang chao <chang-chao@hotmail.com>
2017-06-23 17:35:17 +0900

    readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Reference:
 1. http://musigma.org/java/2016/11/21/reactor.html
 1. https://stackoverflow.com/questions/42299455/spring-webflux-and-reading-from-database
 1. https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples/spring-boot-sample-data-jpa
+1. http://www.grahamlea.com/2014/07/rxjava-threading-examples/
+1. https://spring.io/blog/2016/07/20/notes-on-reactive-programming-part-iii-a-simple-http-server-application
--- src/main/java/me/changchao/spring/springwebfluxasyncjdbcsample/web/CityController.java
+++ src/main/java/me/changchao/spring/springwebfluxasyncjdbcsample/web/CityController.java
@@ -9,8 +9,10 @@ import me.changchao.spring.springwebfluxasyncjdbcsample.domain.City;
 import me.changchao.spring.springwebfluxasyncjdbcsample.service.CityService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Controller
+
 public class CityController {
 	@Autowired
 	private CityService cityService;
@@ -25,6 +27,35 @@ public class CityController {
 	@ResponseBody
 	public Flux<City> list() {
 		return this.cityService.findAll();
+	}
+
+	@GetMapping("/reactor")
+	@ResponseBody
+	String reactor() {
+
+		 Flux.just("a","b","c").log()
+//		Flux.from((s) -> {
+//			System.out.println("subscribe " + Thread.currentThread().getName());
+//			
+//			s.onNext("a");
+//			s.onNext("b");
+//			s.onNext("c");
+//			s.onComplete();
+//		
+//		}) // this is where subscription triggers data
+			// production
+			// this is influenced by subscribeOn
+				.doOnNext(v -> System.out
+						.println("doOnNext before publish " + v + " on " + Thread.currentThread().getName()))
+				.publishOn(Schedulers.elastic())
+				// the rest is influenced by publishOn
+				.doOnNext(v -> System.out
+						.println("doOnNext after publish " + v + " on " + Thread.currentThread().getName()))
+				.subscribeOn(Schedulers.parallel()).subscribe(
+						v -> System.out.println("subscribe received " + v + " on " + Thread.currentThread().getName()))
+
+		;
+		return "OK";
 	}
 
 }
