fab67c23a3ab74e7a8662c992a9ab4dc2c20aadd
chao.chang <chao.chang@linecorp.com>
2018-09-26 11:50:42 +0900

    Use subscribeOn instead of publishOn.

--- src/main/java/me/changchao/spring/springwebfluxasyncjdbcsample/service/CityServiceImpl.java
+++ src/main/java/me/changchao/spring/springwebfluxasyncjdbcsample/service/CityServiceImpl.java
@@ -25,19 +25,15 @@ class CityServiceImpl implements CityService {
 
 	@Override
 	public Mono<City> getCity(String name, String country) {
-		// city or city2,which is the better way?
 		Mono<City> city = Mono
-				.fromCallable(() -> this.cityRepository.findByNameAndCountryAllIgnoringCase(name, country))
-				.publishOn(jdbcScheduler);
-
-		Mono<City> city2 = Mono
 				.defer(() -> Mono.just(this.cityRepository.findByNameAndCountryAllIgnoringCase(name, country)))
 				.subscribeOn(jdbcScheduler);
 
-		// https://spring.io/blog/2016/07/20/notes-on-reactive-programming-part-iii-a-simple-http-server-application
-		Mono<City> city3 = Mono
-				.fromCallable(() -> this.cityRepository.findByNameAndCountryAllIgnoringCase(name, country))
-				.subscribeOn(jdbcScheduler);
+		// Alternative to Mono.defer(). we can also use Mono.fromCallable().
+		// Reference: https://spring.io/blog/2016/07/20/notes-on-reactive-programming-part-iii-a-simple-http-server-application
+//		Mono<City> city = Mono
+//				.fromCallable(() -> this.cityRepository.findByNameAndCountryAllIgnoringCase(name, country))
+//				.subscribeOn(jdbcScheduler);
 
 		return city;
 	}
